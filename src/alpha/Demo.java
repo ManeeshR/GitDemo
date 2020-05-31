e0d9785a331c780bb4160cb794fa6c36b05df702
Maneesh <ranjanmani020@gmail.com>
2020-05-31 20:43:45 +0530

    first commit

---
+++ src/alpha/Demo.java
@@ -0,0 +1,21 @@
+package alpha;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Demo {
+	
+	private static Logger log = LogManager.getLogger(Demo.class.getName());		
+
+	public static void main(String[] args) {
+	
+		log.debug("i am debugg");
+		log.info("hello info");
+		//if(1>2)
+			log.error("here we hav some errors");
+		log.fatal("Fatal Fatal");
+		
+		
+		
+	}
+}
